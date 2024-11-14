60098f8c09edd6d60118d0988352e64074bc6be7
Alan Abdiel <Alan Abdiel@LAPTOP-DA4AJN7D>
2024-11-14 12:12:29 -0600

    Agregar archivos de proyecto.

---
+++ Practica del foco/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Practica_del_foco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Practica_del_foco.Properties.Resources.foco_encendido;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(512, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 283);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Practica_del_foco.Properties.Resources.foco_apagado;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(139, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 283);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Foco";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Practica del foco/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO.Ports;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_del_foco
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort Arduino;
+        //SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            Arduino = new System.IO.Ports.SerialPort();
+            Arduino = new SerialPort("COM4", 9600); //Asegurese de que el puerto COM sea correcto
+            Arduino.Open();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Arduino.Write("F");//Enviar señal para apagar el LED
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Arduino.Write("E");//Enviar señal para encender el LED
+        }
+    }
+}
